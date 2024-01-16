1e49ad750483a4cea035039284bbc826a67241e0
unknown <pkparveenbarak@gmail.com>
2024-01-16 15:49:48 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,18 @@
+let todoinput= document.getElementById("todo");
+let text= document.querySelector(".text");
+function todoadd(){
+    if(todoinput.value==""){
+        alert("please add some task")
+    }
+    else{
+        let list= document.createElement("ul");
+        list.innerHTML=`${todoinput.value} <i style="font-size:24px" class="fa">&#xf014;</i>`;// create element
+        text.appendChild(list); // show element
+todoinput.value="";
+list.querySelector("i").addEventListener("click", remove);
+function remove(){
+    list.remove();//remove the item from list
+}
+
+    }
+}
